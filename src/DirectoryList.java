001420d33b9c10bdb0f58d06befa38811a0cd7ef
RubenAriasM <rubenarmar2005@gmail.com>
2024-10-02 13:25:15 +0200

    Cosas con files

---
+++ src/DirectoryList.java
@@ -0,0 +1,42 @@
+import java.io.File;
+
+public class DirectoryList {
+    public static void main(String[] args) {
+
+        String path = ".";
+
+        if(args.length > 0){
+            path = args[0];
+        }
+
+
+        File file = new File(path);
+
+        if(!file.exists()){
+            System.out.println("File does not exist");
+
+        } else{
+            System.out.println("File exists");
+
+            if(file.isFile()){
+                System.out.println(file + "is a file");
+
+            } else if(file.isDirectory()){
+                System.out.println(file + "is a directory");
+                File[] arrayFiles = file.listFiles();
+
+                for(File f : arrayFiles){
+
+                    String descripcion = f.isFile()? "-" : "/";
+                    System.out.println(descripcion + f.getName());
+                }
+
+            }
+        }
+
+        System.out.println(path);
+
+
+
+    }
+}
